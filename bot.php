b6275f0c052b6c55991924bf5d30fd74561a6265
sekret666 <66795778+sekret666@users.noreply.github.com>
2021-01-31 17:50:38 +0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,7 +9,7 @@ error_reporting(0);
 set_time_limit(0);
 
 flush();
-$API_KEY = 'Your bot token'; //Your token
+$API_KEY = '1628103089:AAGJ_qnF47gvoqUgt0mK7zKnIPv8kjQV6h4'; //Your token
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
